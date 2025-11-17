e5990029fa1c841c092772e65e13a7c44c428ade
AhsokaTano26 <148176849+AhsokaTano26@users.noreply.github.com>
2025-11-18 00:50:11 +0800

    修改群组导入方式

--- src/plugins/summary_bot/config.py
+++ src/plugins/summary_bot/config.py
@@ -1,9 +1,10 @@
 import os
+import ast
 from pydantic import BaseModel
 
 
 class Config(BaseModel):
     """Plugin Config Here"""
     ignore_users: list = [971852256, 2438868634]
-    target_group: list = os.getenv('GROUP_LIST', "None")
+    target_group: list = ast.literal_eval(os.getenv('GROUP_LIST', "None"))
     # target_group: list = [1016925587,339834885,1017112832,962870444,825771260]
